f25e47bdc4376bd3da5a30305bb5bedccc7e9aef
Damola Adeagbo <wetals@hotmail.co.uk>
2018-09-09 20:16:12 +0100

    Exercise 3: Toggle Sidebar

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 $(function () {
+    var main = $(".main");
+
+    $(".navigation__sidebar").on("click", function (event) {
+        event.preventDefault();
+        main.hasClass("sidebar-active") ? main.removeClass("sidebar-active") : main.addClass("sidebar-active");
+    });
 
 });
